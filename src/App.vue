154614856ca58dba7562da1884d70047d39eb0ea
XJkey <1193695252@qq.com>
2023-01-03 12:32:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,157 @@
+<template>
+  <div id="container" ref="container"></div>
+  <colorSelect :car="carMaterial"> </colorSelect>
+</template>
+
+<script setup>
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import colorSelect from "./components/colorSelect.vue";
+
+import { ref, onMounted, reactive } from "vue";
+const container = ref(null);
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#ccc");
+scene.environment = new THREE.Color("#ccc");
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+camera.position.set(-5, 3, 3);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor("#000");
+let control = null;
+
+const light1 = new THREE.DirectionalLight("#fff", 1);
+light1.position.set(0, 0, 10);
+scene.add(light1);
+const light2 = new THREE.DirectionalLight("#fff", 1);
+light2.position.set(0, 0, -10);
+scene.add(light2);
+const light3 = new THREE.DirectionalLight("#fff", 1);
+light3.position.set(10, 0, 0);
+scene.add(light3);
+const light4 = new THREE.DirectionalLight("#fff", 1);
+light4.position.set(-10, 0, 0);
+scene.add(light4);
+const light5 = new THREE.DirectionalLight("#fff", 1);
+light5.position.set(0, 10, 0);
+scene.add(light5);
+const light6 = new THREE.DirectionalLight("#fff", 1);
+light6.position.set(0, 5, 0);
+scene.add(light6);
+
+const grid = new THREE.GridHelper(10, 10);
+grid.material.opacity = 0.2;
+grid.material.transparent = true;
+scene.add(grid);
+
+const gltfLoader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/gltf/");
+gltfLoader.setDRACOLoader(dracoLoader);
+let wheels = [];
+let carBody, fontCar, hoodCar, glassCar;
+const bodyMaterial = new THREE.MeshPhysicalMaterial({
+  color: "gray",
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const fontMaterial = new THREE.MeshPhysicalMaterial({
+  color: "gray",
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const hoodMaterial = new THREE.MeshPhysicalMaterial({
+  color: "gray",
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const wheelsMaterial = new THREE.MeshPhysicalMaterial({
+  color: "gray",
+  metalness: 1,
+  roughness: 0.1,
+});
+
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xffffff,
+  metalness: 0,
+  roughness: 0,
+  transmission: 1,
+  transparent: true,
+});
+let carMaterial = [bodyMaterial, fontMaterial, hoodMaterial, wheelsMaterial];
+
+gltfLoader.load("/model/bmw01.glb", (gltf) => {
+  let bmw = gltf.scene;
+  bmw.scale.set(2, 2, 2);
+  //console.log(bmw);
+  bmw.traverse((child) => {
+    if (child.isMesh) {
+      //console.log(child.name);
+      if (child.name.includes("轮毂")) {
+        child.material = wheelsMaterial;
+        wheels.push(child);
+      }
+      if (child.name.includes("Mesh002")) {
+        carBody = child;
+        carBody.material = bodyMaterial;
+      }
+      if (child.name.includes("前脸")) {
+        fontCar = child;
+        fontCar.material = fontMaterial;
+      }
+      if (child.name.includes("引擎盖_1")) {
+        hoodCar = child;
+        hoodCar.material = hoodMaterial;
+      }
+      if (child.name.includes("挡风玻璃")) {
+        glassCar = child;
+        glassCar.material = glassMaterial;
+      }
+    }
+  });
+  scene.add(bmw);
+});
+const clock = new THREE.Clock();
+const render = () => {
+  renderer.render(scene, camera);
+  control.update();
+  let elapsedTim = clock.getElapsedTime() / 2;
+  camera.position.x = 8 * Math.sin(elapsedTim);
+  camera.position.z = 8 * Math.cos(elapsedTim);
+  requestAnimationFrame(render);
+};
+
+onMounted(() => {
+  control = new OrbitControls(camera, renderer.domElement);
+  //control.update();
+  control.enabled = false;
+  container.value.appendChild(renderer.domElement);
+  render();
+});
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+</script>
+
+
+<style scoped>
+#container {
+  width: 100vw;
+  height: 100vh;
+  background-color: aqua;
+}
+</style>
---
+++ src/components/colorSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="colorSelect">
+    <span class="select" v-for="(item, index) in colors" :key="index" :style="{ backgroundColor: item }" @click="changeColor(item)"></span>
+  </div>
+</template>
+<script setup>
+let colors = ["brown", "gray", "cadetblue"];
+const props = defineProps(["car"]);
+const changeColor = (color) => {
+  let car = props.car;
+  for (let item of car) {
+    item.color.set(color);
+  }
+};
+</script>
+<style scoped>
+#colorSelect {
+  position: absolute;
+  bottom: 150px;
+  left: -135px;
+  width: 270px;
+  margin-left: 50%;
+}
+
+.select {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  /* background-color:cadetblue; */
+  display: inline-block;
+  margin: 0 30px;
+  cursor: pointer;
+}
+</style>
